37e0e3b43ed313a243213cc8e9e0964706052e30
Jeremy Scemama <jeremyscemama3@gmail.com>
2018-05-30 16:16:06 +0300

    adding elements to the todo List

--- js/todo.js
+++ js/todo.js
@@ -1,12 +1,48 @@
+// var data ={
+//     myList:[]
+// }
+
 class App extends React.Component{
     constructor(props){
         super(props);
+        this.addToDo = this.addToDo.bind(this);
+        this.state={
+            myList:[]
+        }
+    }
+
+    addToDo(){
+        this.state.myList.push(this.textInput.value)
+        this.setState({
+            myList:this.state.myList    
+        })
+        
+    }
+
+    renderMap(arr){
+        return arr.map(x=><div key ={`item${x}`}>{x}</div>);
     }
     render(){
+       
         return(
             <div>
                 <h1>ToDo List</h1>
-                   
+
+                <div id ="toDo">
+                    <input ref={(input)=>{
+                        this.textInput=input;
+                    }}/>
+                    <button onClick={this.addToDo}>add</button>
+
+                    <div id="list">
+                        {this.renderMap(this.state.myList)}
+                    
+                    </div>
+                </div>
+
+                <div id ="bom">
+                
+                </div>
             </div>
         )
     }
