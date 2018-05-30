bffac1a7f15667fc8cf57aba1fd3c9c54e37039c
Jeremy Scemama <jeremyscemama3@gmail.com>
2018-05-30 14:01:43 +0300

    todo started

---
+++ js/todo.js
@@ -0,0 +1,20 @@
+class App extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div>
+                <h1>ToDo List</h1>
+                   
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById("root")
+);
+
+
